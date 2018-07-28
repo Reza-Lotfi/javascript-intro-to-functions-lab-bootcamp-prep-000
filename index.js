653bf9f092dddf7a89d930b219bd71105b51658e
Reza Lotfi <rezaiscool7@aol.com>
2018-07-28 02:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if (string.toUpperCase() === string)
-    console.log("YES INDEED!")
+    console.log('YES INDEED!')
     
   if (string.toLowerCase() === string)
-    console.log("I can\'t hear you!")
+    console.log('I can\'t hear you!')
   
-  if (string === "I love you, Grandma.")
-    console.log("I love you, too.")
+  if (string === 'I love you, Grandma.')
+    console.log('I love you, too.')
 }
