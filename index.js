440e767062216c33f36a787952b8715910a7d10e
Reza Lotfi <rezaiscool7@aol.com>
2018-07-28 01:58:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if `string`.toUpperCase() === uppercase
+  if `string`.toUpperCase() === `string`
     {console.log('YES INDEED!')}
-  if `string`.toLowerCase() === lowercase
+  if `string`.toLowerCase() === `string`
     {console.log("I can't hear you!")}
   if `string` === 'I love you, Grandma.'
     {console.log('I love you, too.')
